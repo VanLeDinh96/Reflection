be4bfda4b89a23555a304c879a61eeddd04bd938
VanLeDinh96 <dinhvanle.it@gmail.com>
2024-09-06 00:00:13 +0700

    Add project files.

---
+++ Reflection/MyClass.cs
@@ -0,0 +1,10 @@
+﻿namespace Reflection;
+
+internal class MyClass
+{
+    public string Name { get; set; }
+    public void PrintMessage()
+    {
+        Console.WriteLine("Hello from MyClass!");
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,11 @@
+﻿using Reflection;
+using System.Reflection;
+
+Type type = typeof(MyClass);
+Console.WriteLine("Class: " + type.Name);
+
+MethodInfo[] methods = type.GetMethods();
+foreach (var method in methods)
+{
+    Console.WriteLine("Method: " + method.Name);
+}
